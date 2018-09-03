a39d22886924a82df3d8a451b158196b418d2ecb
Sixian Yang <smartyyy@mail.ustc.edu.cn>
2018-09-03 19:02:45 +0200

    upload GUI via Tkinter, initial version

---
+++ tkdocs_tutorial/nomad-repo-uploadGUI.py
@@ -0,0 +1,73 @@
+from Tkinter import *
+import ttk
+import tkFileDialog
+import tkMessageBox
+
+###root window
+root =Tk()
+root.configure(bg='lightgrey')
+root.title('Select the files to upload')
+#root.geometry('680x400')
+
+lbl1 = Label(root, text='file list', font=('Helvetica', 16, 'bold'))
+lbl1.grid(row=0, column=0, sticky=W)
+
+#funtions:open a filedialog window,select files and add it to the listbox
+
+uploadfilename_dict = {}
+def file_directory_get(*args):
+    file_directory = tkFileDialog.askopenfilename(initialdir = '/', title='Select file', \
+                                                  filetypes = (("zip files","*.zip"),("tar files","*.tar.gz"),("all files","*.*")))
+    try:
+        uploadfilename_dict[file_directory.split('/')[-1]]
+    except KeyError:
+        uploadfilename_dict[file_directory.split('/')[-1]] = file_directory #file_directory.split('/')[-1] is the file name
+        listfile.insert(END, file_directory.split('/')[-1])
+    else:
+        tkMessageBox.showinfo("Warning", "File already exists in the list!")#Add a existing file would accept a warning
+
+def remove_selected_listitem():
+    #current_selction = listfile.curselection()
+    listfile.delete(ANCHOR)
+    
+
+
+#frame1:filelist and scrollbar
+frm1 = Frame(root)#padding=(5, 5, 12, 0))
+frm1.grid(row=1, column=0, sticky=(N,S))
+root.rowconfigure(1, weight=1)
+root.columnconfigure(1, weight=1)
+
+scrollbar = Scrollbar(frm1, orient=VERTICAL)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+
+#fnames=StringVar(value=selectedfilenames)
+listfile = Listbox(frm1, width=20, yscrollcommand=scrollbar.set, font=('Helvetica', 12))
+listfile.pack(expand=True, fill=Y)
+
+scrollbar.config(command=listfile.yview)
+
+#frame2:buttons
+#frm2 = Frame(root)
+#frm2.grid(row=1, column=1, sticky=(N,S,E,W))
+#root.rowconfigure(2, weight=1)
+#root.columnconfigure(2, weight=1)
+#prsent not use frame2
+#Buttons
+add_button = Button(root, text='Add',command=file_directory_get, font=('Helvetica', 8), width=6, height=1)
+remove_button = Button(root, text='Remove',command=remove_selected_listitem, font=('Helvetica', 8), width=6, height=1)
+upload_button = Button(root, text='Upload',font=('Helvetica', 8), width=6, height=1)#command=??
+exit_button = Button(root, text='Cancel', command=root.quit, font=('Helvetica', 8), width=6, height=1)
+
+add_button.grid(row=0, column=1, padx=5)
+remove_button.grid(row=1, column=1, padx=5, pady=2)
+upload_button.grid(row=2, column=1, padx=5, pady=6)
+exit_button.grid(row=3, column=1, padx=5, pady=6)
+
+#for x in range(100):
+#    tmp = 'filename' + str(x)
+#    listfile.insert(END, tmp)
+
+
+root.mainloop()
